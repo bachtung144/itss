2ec7d3870d9ee27fee5b9ddcfa5fc0dd1dd6b89d
bachtung144 <bachtung144007@gmail.com>
2021-09-08 10:49:35 +0700

    final

---
+++ src/customHook.js
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+function CustomHook() {
+    const [name, setName] = useState('')
+    const [arr, setArr] = useState(['Huyen', 'Hoa', 'Hung', 'Long'])
+    const handleChange= (e) => {
+        setName(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        let tmp = arr;tmp.push(name);setArr(tmp);e.preventDefault();
+    }
+    return (
+        <div>
+            <p>学生一覧: [{arr.map((item) => item + ',')}]</p>
+            <p>追加する名前を入力してください</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input type="text" value={name} onChange={handleChange} /></label>
+                <input type="submit" value="確定" />
+            </form>
+            <p>追加する名前: {name}</p>
+            <p>新しい一覧: [{arr.map((item) => item + ',')}]</p>
+        </div>
+    );
+}
+export default CustomHook;
